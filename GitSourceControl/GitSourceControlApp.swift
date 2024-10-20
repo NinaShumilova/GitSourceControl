8d77670fe0f6bfd6c360da95abb759ccc1b4cb44
Nina Shumilova <shumiliha@gmail.com>
2024-10-20 17:23:46 +0800

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by nina on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
