fe8d4228b28f926c42623cb80d9eabeb295325ae
Nina Shumilova <shumiliha@gmail.com>
2024-10-21 15:23:20 +0800

    [Clean] Add commit message documentation

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by nina on 20.10.2024.
 //
 
+/*
+ 
+ COMMIT MESSAGES
+ 
+ NEW FUTURE:
+ [Feature] Description of the future
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+ 
 import SwiftUI
 
 struct ContentView: View {
