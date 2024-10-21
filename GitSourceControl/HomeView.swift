fc9ab2457b56a90a3da13c478f70ad6dbe59ed77
Nina Shumilova <shumiliha@gmail.com>
2024-10-21 15:04:41 +0800

    Add HomeView

---
+++ GitSourceControl/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GitSourceControl
+//
+//  Created by nina on 21.10.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
