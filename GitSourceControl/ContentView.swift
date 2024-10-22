be6827aa0d14a7f78fd45f8df803c106195edd2e
Nina Shumilova <shumiliha@gmail.com>
2024-10-22 14:58:44 +0800

    [Bug] Swiftful Thinking in Text

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
                 .font(.largeTitle)
         
                 .foregroundColor(.accentColor)
-            Text("Swiftful!c")
+            Text("Swiftful Thinking!")
             Button("Subscribe") {
                 
             }
