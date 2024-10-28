637cdc24a1c87b2e3bcb4155c509441029c66d35
Nina Shumilova <shumiliha@gmail.com>
2024-10-28 14:18:44 +0800

    [Feature] Fix text

--- GitSourceControl/HomeView.swift
+++ GitSourceControl/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-        Text("Hello!")
+        Text("Hello Nina!")
             Text("Screen 2!")
            
     }
