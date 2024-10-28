2d26b1e4c90d15598c184ba8b21d13f603bd8c9d
Nina Shumilova <shumiliha@gmail.com>
2024-10-28 14:14:26 +0800

    [Feature] Click me

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
         
                 .foregroundColor(.accentColor)
             Text("Swiftful Thinking!")
-            Button("Subscribe now!") {
+            Button("Clock me!") {
                 
             }
             .background(Color.red)
