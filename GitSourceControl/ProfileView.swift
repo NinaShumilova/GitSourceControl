0b94439e6bcb3e7f3d75ccb1f6e0db000c1b624d
Nina Shumilova <shumiliha@gmail.com>
2024-11-03 16:40:58 +0800

    [Feature] Add Profileview

---
+++ GitSourceControl/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  GitSourceControl
+//
+//  Created by nina on 03.11.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nina")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
